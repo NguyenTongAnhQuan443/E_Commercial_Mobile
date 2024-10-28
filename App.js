5b8ec82dee387f2fc17b32fe39e2f5911b4ccc02
NguyenThanhNhut13 <thanhnhutcu@gmail.com>
2024-10-28 20:09:15 +0700

    add Home Screen

--- App.js
+++ App.js
@@ -1,19 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-import Login from './componets/Login';
-import SignUp from './componets/SignUp';
-import HomeDashboard from './componets/HomeDashboard';
-import HomeDashboardNew from './componets/HomeDashboardNew';
+import HomeDashboardNew from './Componets/HomeDashboardNew';
 
 import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    // <Login/>
-    // <SignUp/>
-    // <HomeDashboard/>
-
     <NavigationContainer>
       <HomeDashboardNew />
     </NavigationContainer>
--- Componets/HomeDashboardNew.js
+++ Componets/HomeDashboardNew.js
@@ -4,9 +4,12 @@ import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { MaterialIcons } from '@expo/vector-icons';
 
-import Login from './Login';
-import SignUp from './SignUp'; 
+import SignUp from './SignUp';
 import Explore from './Explore';
+import HomeScreen from './HomeScreen';
+
+import Icon from 'react-native-vector-icons/Feather';
+
 const HomeDashboardNew = () => {
 
     const Tab = createBottomTabNavigator();
@@ -17,23 +20,38 @@ const HomeDashboardNew = () => {
                 tabBarIcon: ({focused, color, size}) => {
                     let iconName;
 
-                    if(route.name == 'Home'){
-                        iconName = focused ? 'home' : 'home-outline';
-                    }else if(route.name == 'Home1'){
-                        iconName = focused ? 'person-add' : 'person-add-alt-1';
+                    // if(route.name == 'Home'){
+                    //     iconName = focused ? 'home' : 'home-outline';
+                    // }else if(route.name == 'Home1'){
+                    //     iconName = focused ? 'person-add' : 'person-add-alt-1';
 
-                        return <MaterialIcons name = {iconName} size={size} color={color}/>
-                    }
-                }
+                    //     return <MaterialIcons name = {iconName} size={size} color={color}/>
+                    // }
+                    if (route.name === 'Home') {
+                        iconName = 'home'; 
+                      } else if (route.name === 'Signup') {
+                        iconName = 'user-plus'; 
+                      } else if (route.name === 'Explore') {
+                        iconName = 'search'; 
+                      }
+            
+                      return (
+                        <Icon
+                          name={iconName}
+                          size={size} 
+                          color={color} 
+                          style={{ padding: 5 }} 
+                        />
+                      );
+                },
+                tabBarActiveTintColor: '#53b175',
+                headerShown: false,
             })}
         >
-            <Tab.Screen name='Home' component={Explore} />
-            <Tab.Screen name='Home1' component={SignUp}/>
+            <Tab.Screen name='Home' component={HomeScreen} />
+            <Tab.Screen name='Explore' component={Explore} />
+            <Tab.Screen name='Signup' component={SignUp}/>
         </Tab.Navigator>
-
-        // <View></View>
-
-        // <Login/>
     )
 }
 
